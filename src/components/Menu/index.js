fff28e607a5371298b285cc7db6f69959d987052
Thiago Cedran Santos <thiagocdn@gmail.com>
2020-07-27 20:38:59 -0300

    Iniciado header

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
+import ButtonLink from '../components/ButtonLink';
+import Logo from '../../assets/images/logo.png'
+import './styles.css'
+
 function Menu () {
   return (
-    <nav>
-      TCFLIX
+    <nav className="Menu">
+      <a href="/">
+        <img className="Logo" src={Logo} alt="Logo TCFlix"/>
+      </a>
+
+      <ButtonLink className="ButtonLink" href="/">
+        Novo Vídeo
+      </ButtonLink>
     </nav>
   );
 }
